e692d1829324d0487967860638bab5898ba050ed
Johan Swan <jswannieman@gmail.com>
2024-04-09 11:34:54 +1200

    Commit searchPeople JSX Component

---
+++ src/SearchPeople.jsx
@@ -0,0 +1,17 @@
+import { memo } from "react";
+
+function SearchPeople({ handleSearch }) {
+  console.log("Search Rendered");
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="search for people"
+        onChange={(event) => handleSearch(event.target.value)}
+        className="border-4 border-black-950 bg-blue-600 text-white p-2 mb-8"
+      />
+    </div>
+  );
+}
+
+export default memo(SearchPeople);
